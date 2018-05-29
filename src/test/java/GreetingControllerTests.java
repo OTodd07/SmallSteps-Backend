dcc36c7cbc45b4479df95a44d614161861a2de08
oft16 <oft16@ic.ac.uk>
2018-05-29 22:39:17 +0100

    Initial commit of hello-world backend API with gradle build and test set up

---
+++ src/test/java/GreetingControllerTests.java
@@ -0,0 +1,14 @@
+import api.GreetingController;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class GreetingControllerTests {
+
+  @Test
+  public void noArgumentsGivesHelloWorld() {
+    GreetingController controller = new GreetingController();
+    assertTrue(controller.greeting("hello").getContent().equals("Hello, HELLO!"));
+  }
+}
