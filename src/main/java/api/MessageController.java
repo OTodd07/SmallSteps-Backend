78eb3805bb47f02178fc4d537d86fdb262eb462c
oft16 <oft16@ic.ac.uk>
2018-06-06 13:42:09 +0100

    Added WebSocketConfig, Messages and MessageController

---
+++ src/main/java/api/MessageController.java
@@ -0,0 +1,18 @@
+package api;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MessageController {
+
+
+  @MessageMapping("/notifyWalkers")
+  @SendTo("/all/messages")
+  public OutputMessage send(Message message) throws Exception {
+    String time = new SimpleDateFormat("HH:mm").format(new Date());
+    return new OutputMessage(message.getFrom(), message.getText(), time);
+  }
+}
---
+++ src/main/java/api/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketHandlerRegistration;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/notifyWalkers");
+    registry.addEndpoint("/notifyWalkers").withSockJS();
+  }
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry config) {
+    config.enableSimpleBroker("/all");
+    config.setApplicationDestinationPrefixes("/app");
+  }
+}
