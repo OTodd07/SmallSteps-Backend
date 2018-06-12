9e09d3326fc0ba2e69b82590e5d14637996cf043
oft16 <oft16@ic.ac.uk>
2018-06-12 15:20:43 +0100

    Set up web socket without using stomp

--- src/main/java/api/WebSocketConfig.java
+++ src/main/java/api/WebSocketConfig.java
@@ -12,11 +12,11 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
-  @Override
-  public void configureMessageBroker(MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/all");
-    config.setApplicationDestinationPrefixes("/app");
-  }
+//  @Override
+//  public void configureMessageBroker(MessageBrokerRegistry config) {
+//    config.enableSimpleBroker("/all");
+//    config.setApplicationDestinationPrefixes("/app");
+//  }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
