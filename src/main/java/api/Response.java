83c3b52fb8e560da99cd3ebec6d3c0081a919708
oft16 <oft16@ic.ac.uk>
2018-06-14 12:05:20 +0100

    Moved out location from response and replaced with components

--- src/main/java/api/Response.java
+++ src/main/java/api/Response.java
@@ -9,23 +9,36 @@ public class Response {
   private boolean response;
 
   @JsonProperty
-  private Location location;
+  private String latitude;
+
+  @JsonProperty
+  private String longitude;
 
   @JsonCreator
-  public Response(@JsonProperty("response") boolean response, @JsonProperty("location") Location location) {
+  public Response(@JsonProperty("response") boolean response, @JsonProperty("latitude") String latitude, @JsonProperty("longitude") String longitude) {
     this.response = response;
-  }
-
-  public Location getLocation() {
-    return location;
-  }
-
-  public void setLocation(Location location) {
-    this.location = location;
+    this.latitude = latitude;
+    this.longitude = longitude;
   }
 
   public boolean isResponse() {
     return response;
+  }
+
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
   }
 
   public void setResponse(boolean response) {
