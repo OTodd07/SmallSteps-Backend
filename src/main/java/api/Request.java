543ac2ddc10873cdc8caf150f9d7d94ae1a1c227
oft16 <oft16@ic.ac.uk>
2018-06-14 21:57:25 +0100

    Added lat and long to request object

--- src/main/java/api/Request.java
+++ src/main/java/api/Request.java
@@ -8,16 +8,42 @@ public class Request {
   @JsonProperty
   private String sender;
 
+  @JsonProperty
+  private String senderLat;
+
+  @JsonProperty
+  private String senderLong;
+
   @JsonCreator
-  public Request(@JsonProperty("sender") String sender) {
+  public Request(@JsonProperty("sender") String sender, @JsonProperty("senderLat") String senderLat,
+                 @JsonProperty("senderLong") String senderLong) {
     this.sender = sender;
+    this.senderLat = senderLat;
+    this.senderLong = senderLong;
   }
 
   public String getSender() {
     return sender;
   }
 
-  public void setSender(String sender) {
+  public void setSender(String sender)
+  {
     this.sender = sender;
+  }
+
+  public String getSenderLat() {
+    return senderLat;
+  }
+
+  public void setSenderLat(String senderLat) {
+    this.senderLat = senderLat;
+  }
+
+  public String getSenderLong() {
+    return senderLong;
+  }
+
+  public void setSenderLong(String senderLong) {
+    this.senderLong = senderLong;
   }
 }
